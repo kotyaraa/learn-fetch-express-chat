a5bd63687d324c181fde17ffb3b0fd7d2f77c174
kotyaraa <42804223+kotyaraa@users.noreply.github.com>
2020-10-14 18:21:49 +0300

    finish

--- server.js
+++ server.js
@@ -1,39 +1,64 @@
+
+const cors = require('cors');
 const express = require('express');
 const app = express();
-express.json()
 const fs = require('fs');
-const port = 3000
+const { v4: uuidv4 } = require('uuid');
+app.use(cors());
+app.use(express.static(__dirname + "/public"))
 
-// app.use(function (q, r) { 
-//     if (q.url === '/favicon.ico') {
-//       r.writeHead(200, {'Content-Type': 'image/x-icon'} );
-//       r.end();
-//       console.log('favicon requested');
-//       return;
-//     }
-//    })
+let history = require('./history.json') || [];
+
+app.use(function (q, r) { 
+    if (q.url === '/favicon.ico') {
+      r.writeHead(200, {'Content-Type': 'image/x-icon'} );
+      r.end();
+      console.log('favicon requested');
+      return;
+    }
+   })
+
 app.use(express.json());
-
-let history = require("./history.json") || [];
-
 app.get('/', (req, res) => {
-  res.send(
-      fs.readFileSync('index.html',{encoding: 'utf-8'})
-  )
-})
+    res.send(
+        fs.readFileSync("index.html", {encoding: 'utf-8'})
+    )
+    })
 app.post('/history', (req, res) => {
-    
+    req.body.id = uuidv4();
     history.push(req.body)
     fs.writeFileSync("./history.json", JSON.stringify(history))
     res.send(history);
 })
+app.delete('/history', (req, res) => {
+    history = [];
+    fs.writeFileSync("./history.json",JSON.stringify(history));
+    res.send(history);
+})
+app.delete('/history/:id', (req, res) => {
+    let deleteId = req.params.id;
+    let newArr = [];
+   for(let item of history){
+    if(item.id !== deleteId){
+        newArr.push(item);
+    }
+   }
+   history = newArr;
+   fs.writeFileSync("./history.json", JSON.stringify(history))
+   res.send(history);
+})
 app.put("/history/:id", async (req, res) => {
-    
+    const editId = req.params.id;
     let editMsg = req.body.message;
-    
+    for(let item of history){
+        if(item.id === editId){
+            item.message = editMsg;
+        }
+    }
     fs.writeFileSync("./history.json", JSON.stringify(history))
     res.send(history);
 })
 
+app.listen(8080)
 
-app.listen(port)
+
